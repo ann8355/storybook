6dab9edde69561d51045d497360a70f4d26c2b48
parry <parry.lin@presslogic.com>
2019-05-09 14:14:24 +0800

    架構調整。

--- story/components/circleProcess/index.stories.js
+++ story/components/circleProcess/index.stories.js
@@ -1,22 +1,19 @@
 import { storiesOf } from '@storybook/vue';
-import { withKnobs, color, boolean, number } from '@storybook/addon-knobs';
-import myComponent from './cirleProcess.vue';
+import { withKnobs, color, number } from '@storybook/addon-knobs';
+import myComponent from './circleProgressBar.vue';
 
-const stories = storiesOf('Components|cirle-process', module);
+const stories = storiesOf('Components|ProgressBar', module);
 stories.addDecorator(withKnobs);
 stories.add(
-    'cirle-process',
+    'Circle-ProgressBar',
     () => ({
         components: { myComponent },
         props: {
-            isShow: {
-                default: boolean('isShow', true)
-            },
             width: {
                 default: number('width', 200)
             },
             radius: {
-                default: number('radius', 20)
+                default: number('radius', 10)
             },
             progress: {
                 default: number('progress', 70)
@@ -27,14 +24,17 @@ stories.add(
             delay: {
                 default: number('delay', 20)
             },
-            barColor: {
-                default: color('barColor', '#F2AE57')
+            colorStart: {
+                default: color('colorStart', '#ffc16f')
+            },
+            colorEnd: {
+                default: color('colorEnd', '#ffe191')
             },
             backgroundColor: {
-                default: color('backgroundColor', '#FFE8CC')
+                default: color('backgroundColor', '#ededed')
             }
         },
-        template: `<myComponent :isShow="isShow" :width="width" :radius="radius" :progress="progress" :duration="duration" :delay="delay" :barColor="barColor" :backgroundColor="backgroundColor" />`
+        template: `<myComponent :width="width" :radius="radius" :progress="progress" :duration="duration" :delay="delay" :colorStart="colorStart" :colorEnd="colorEnd" :backgroundColor="backgroundColor" />`
     }),
     {
         notes: 'vue 版圓形進度條，自帶進度動畫，各項可動式調整 ver 1.0.0',
