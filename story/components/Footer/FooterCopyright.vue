447c2591137105f42b2bd67b4d452544de8541c9
Tim <tim@admins-iMac.local>
2019-06-06 17:59:12 +0800

    PressLogic Footer

---
+++ story/components/Footer/FooterCopyright.vue
@@ -0,0 +1,38 @@
+<template>
+    <div 
+        class="copyright"
+        :style="{ 
+            color: copyrightColor, 
+            background: copyrightBgColor
+        }"
+    >Copyright &copy; {{ new Date().getFullYear() }} PressLogic {{ magazineName }}. All right reserved</div>
+</template>
+
+<script>
+export default {
+    props:{
+        copyrightColor: {
+            type: String,
+            default: 'black'
+        },
+        magazineName: {
+            type: String,
+            default: 'Magazine Name'
+        }
+    },
+    watch: {},
+    computed: {},
+    data() {
+        return {}
+    },
+    mounted () {},
+    destroyed () {},
+    methods: {}
+}
+</script>
+
+<style scoped>
+.copyright {
+    text-align: center;
+}
+</style>
