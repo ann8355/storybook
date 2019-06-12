3d495aeab7f4b9acf184ea22762971078ae0291a
howie <howie.wang@presslogic.com>
2019-06-12 16:57:01 +0800

    revamp header footer mask update

--- story/components/maskRevamp/goBeautyButton.vue
+++ story/components/maskRevamp/goBeautyButton.vue
@@ -1,34 +1,36 @@
 <template>
-<a class="go-to-url" :href="url" :style="{color: btntxtColor,fontSize:`${btnTxtSize}px`}">{{ btnTxt }}<i class="el-icon-right"></i></a>
+  <a class="go-to-url" :href="url" :style="{color: btntxtColor,fontSize:`${btnTxtSize}px`}">
+    {{ btnTxt }}
+    <i class="el-icon-right"></i>
+  </a>
 </template>
 <script>
 export default {
-  props:{
-      url: {
-        type: String,
-        default: 'https://beautybee.com/'
-      },
-      btnTxt: {
-        type: String,
-        default: '前往 BeautyBee'
-      },
-      btntxtColor: {
-				type: String,
-				default: '#fff'
-			},
-			btnTxtSize: {
-				type: Number,
-				default: 20
-			}
-  },
-  data(){
-    return {
+    props: {
+        url: {
+            type: String,
+            default: 'https://beautybee.com/'
+        },
+        btnTxt: {
+            type: String,
+            default: '前往 BeautyBee'
+        },
+        btntxtColor: {
+            type: String,
+            default: '#fff'
+        },
+        btnTxtSize: {
+            type: Number,
+            default: 20
+        }
+    },
+    data() {
+        return {};
     }
-  }
-}
+};
 </script>
 <style scoped>
-.go-to-url{
+.go-to-url {
     display: inline-block;
     box-sizing: border-box;
     background: linear-gradient(to bottom, #ff6987, #ff525a);
@@ -36,7 +38,7 @@ export default {
     height: 50px;
     box-shadow: 0 2px 5px 0 rgba(199, 53, 53, 0.55);
     font-weight: 600;
-	text-align: center;
+    text-align: center;
     border-top-left-radius: 25px;
     border-bottom-left-radius: 25px;
     text-decoration: none;
